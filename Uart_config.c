b122fc6cbe547ef7e367e3bf711f8b5acd6fe717
unknown <g00238234@ENG508.gmadtree.gmit.ie>
2016-01-19 13:41:43 +0000

    uart configuration for uart1

--- Uart_config.c
+++ Uart_config.c
@@ -9,6 +9,8 @@
 #include "board.h"
 #include "fsl_clock_manager.h"
 #include "fsl_debug_console.h"
+#include <stdlib.h>
+#include <string.h>
 
 
 #define RDRF_MASK 0x20	//Receive Data Register Full Flag Mask
@@ -19,7 +21,7 @@
  * Prototypes
  ******************************************************************************/
 char char_received();
-void enable_UART0_receive_interrupt();
+void enable_UART1_receive_interrupt();
 void put_char(char c);
 /*******************************************************************************
  * Code
@@ -29,11 +31,11 @@ void put_char(char c);
  * UART0 Interrupt Handler
  * Echos received character
  */
-void UART0_IRQHandler(void)
+void UART1_IRQHandler(void)
 {
-    if(UART0_S1 & RDRF_MASK)
+    if(UART1_S1 & RDRF_MASK)
     {
-    	PUTCHAR(UART0_D);
+    	PUTCHAR(UART1_D);
     	//PUTCHAR('A');
     }
 }
@@ -41,62 +43,66 @@ void UART0_IRQHandler(void)
 //this is not needed as hardware init executes this for UART
 void uart_config()
 {
-		PORTA_PCR1|=0x2u<<8;//mux - ALT2 UART0_RX
-		PORTA_PCR2|=0x2u<<8;//mux - ALT2 UART0_TX
+		int SBR;
+		PORTE_PCR0|=0x3u<<8;//mux - ALT2 UART0_RX
+		PORTE_PCR1|=0x3u<<8;//mux - ALT2 UART0_TX
 
-		SIM_SCGC4|=SIM_SCGC4_UART0_MASK;//enabling clock
-		UART0_C2&=0XF3;//diabling transmittion and receiving until config complete
+		SIM_SCGC4|=0x1u<<11;//enabling clock
+		UART1_C2&=0XF3;//diabling transmittion and receiving until config complete
 		SIM_SOPT2|=0x1u<<26;
-		uart_clock=CLOCK_SYS_GetPllFllClockFreq();
-		OSR=15;
-		SBR=uart_clock/((OSR + 1)*BAUD_RATE);
-		UART0_C4=OSR;
-		UART0_BDL=SBR&OXFF;
-		UART0_BDH|=((SBR&0XFF00)>>8);
+		//uart_clock=CLOCK_SYS_GetPllFllClockFreq();
+		//OSR=15;
+		SBR=78;
+		//UART1_C4=OSR;
+		UART1_BDL=SBR&0XFF;
+		UART1_BDH|=((SBR&0XFF00)>>8);
 
-		UART0_C1=0;
-		UART_C2|=0X0C;
-		UART0_C3=0;
+		UART1_C1=0;
+		UART1_C2|=0X0C;
+		UART1_C3=0;
 }
 
 
 int main()
 {
+
 	hardware_init();
-	enable_UART0_receive_interrupt();
+	uart_config();
+	enable_UART1_receive_interrupt();
 
 	PRINTF("UART0 Test Code\n\r");
 	PRINTF("Any entered character will be echoed\r\n\n");
 
 	while(1)
 	{
-
+		put_char(0x55);
+		for(int x=0;x<10000;x++);
 	}
 }
 
 
 char char_received()
 {
-	if(UART0_S1 & RDRF_MASK)
+	if(UART1_S1 & RDRF_MASK)
 		return 1;
 	else
 		return 0;
 }
 
 
-void enable_UART0_receive_interrupt()
+void enable_UART1_receive_interrupt()
 {
 	//Configure NVIC
-	NVIC_ClearPendingIRQ(12);
-	NVIC_EnableIRQ(12);
-	UART0_C2 |= RIE_MASK;	//set RIE to enable receive interrupt
+	NVIC_ClearPendingIRQ(13);
+	NVIC_EnableIRQ(13);
+	UART1_C2 |= RIE_MASK;	//set RIE to enable receive interrupt
 }
-//void put_char(char c)
-//{
-	//while((UART0_S1 & TDRE_MASK)==0)
-	//{}
-	//UART0_D=c;
-//}
+void put_char(char c)
+{
+	while((UART1_S1 & UART_S1_TRDE_MASK)==0)
+	{}
+	UART1_D=c;
+}
 
 
 
