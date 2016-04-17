bfa59b5caf2337a469d61aae5f7a29dcb5670f6a
saoirse-stewie <saoirses@hotmail.com>
2016-04-17 21:45:04 +0100

    added second choice for tool

--- Button_led_database.c
+++ Button_led_database.c
@@ -13,30 +13,41 @@ a * LED_test_external_buttons.c
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFERSIZE 10
+#define BUFFERSIZE 20
 #define RDRF_MASK 0x20	//Receive Data Register Full Flag Mask
 #define RIE_MASK 0x20	//Receive Interrupt Enable Mask
 #define UART_S1_TRDE_MASK 0X80
 #define TIME 60
-void myPrintf(float fVal);
-char combo1[] = "CR_MP CR_HP";
+void myPrintf(float fVl);
+char combo1[] = "CR_MP CR_MP CR_HP";
+char combo2[] = "CR_LP CR_HP HU_HK";
 
 
 static int cr_mp[2];
 static int cr_hp[2];
 
+static int cr_lp[2];
+static int cr_hp2[2];
+static int hu_hk[2];
+
 static int reaction_crmp=0;
 static int reaction_crhp=0;
+
+static int reaction_crlp=0;
+static int reaction_crhp2=0;
+static int reaction_huhk=0;
+
 volatile int storage=0;
 volatile int storage2=0;
-
-
+volatile int storage3=0;
 
 static int sw2_count=0;
 static int sw_count=0;
 static int sw3_count=0;
 static int sw4_count=0;
 static int sw5_count=0;
+static int sw6_count=0;
+static int sw7_count=0;
 
 volatile int tick_count=0;
 volatile int delay = 10000;
@@ -54,9 +65,6 @@ int run=0;
 int run2=0;
 
 char buffer[BUFFERSIZE];
-
-
-
 
 
 enum STATES{START,FIRST,SECOND,THIRD};
@@ -79,8 +87,11 @@ void uart_config();
 void database_extraction();
 void char_int_conversion(char c[], int, int);
 double reaction_time(int,int);
+double reaction_time2(int,int,int);
 void PIT_IRQHandler();
 void FloatToStringNew(char *str, float f, char size);
+
+
 int main()
 {
 
@@ -104,6 +115,8 @@ int main()
 	FRDM_KL26Z_SW4_Configure(0,FALLING_EDGE);
 	FRDM_KL26Z_SW5_Configure(0,FALLING_EDGE);
 	FRDM_KL26Z_SW6_Configure(0,FALLING_EDGE);
+	FRDM_KL26Z_SW7_Configure(0,FALLING_EDGE);
+	FRDM_KL26Z_SW8_Configure(0,FALLING_EDGE);
 
 	PRINTF("hello");
 
@@ -129,6 +142,7 @@ int main()
 		case FIRST:
 			sw_count=0;
 			sw2_count=0;
+			sw5_count=0;
 			while(sw2_count==0)
 			{}
 			LED_set(GREEN,ON);
@@ -138,15 +152,26 @@ int main()
 			currentstate = SECOND;
 			break;
 		case SECOND:
-			sw_count=0;
+			//sw_count=0;
 			sw2_count=0;
-			//sw5_count=0;
-			while(sw2_count==0)
-			{}
+			sw5_count=0;
 
-			//tx_string("Combo");
-			//database_extraction(combo1);
-			currentstate = FIRST;
+			if(sw2_count%2!=0)
+			{
+				tx_string("combo");
+				database_extraction(combo1);
+			}
+
+			else if(sw5_count%2!=0)
+			{
+
+				sw2_count=0;
+				while(sw2_count==0)
+				{}
+				tx_string("combo2");
+				database_extraction(combo2);
+			}
+			currentstate = SECOND;
 
 			break;
 		}
@@ -226,9 +251,51 @@ void char_int_conversion(char c[], int reaction_crmp, int reaction_crhp)
 
 
 	}
+	else if(strcmp(c,combo2)==0)//compare both the strings
+	{
+		cr_lp[0] = received_char[0]-'0';//startup//4
+		cr_lp[1] = received_char[4]-'0';//active//4
+		cr_lp[2] = received_char[8]-'0';//recovery //17
+
+		for(i=0;i<3;i++)
+		{
+
+			reaction_crlp = reaction_crlp + cr_lp[i];
+			PRINTF("%d", reaction_crlp);
+			//reaction_crhp = reaction_crhp +
+		}
+		reaction_crlp=(reaction_crlp*1000)/TIME;
+
+		cr_hp2[0] = received_char[1]-'0';//startup//4
+		cr_hp2[1] = received_char[5]-'0';//active//8
+		cr_hp2[2] = received_char[9]-'0';//recovery//20
+		cr_hp2[2] *=10;
+
+		for(i=0;i<3;i++)
+		{
+
+			reaction_crhp2 = reaction_crhp2 + cr_hp2[i];
+		}
+		reaction_crhp2=reaction_crhp2/TIME;
+
+		hu_hk[0] = received_char[3]-'0';
+		//PRINTF("%d",received_char[7]-'0');
+		hu_hk[0] +=10;
+		hu_hk[1] = 10;
+		hu_hk[2] = received_char[10]-'0';
+
+		for(i=0;i<3;i++)
+		{
+			reaction_huhk =reaction_huhk + hu_hk[i];
+		}
+		reaction_huhk=reaction_huhk/TIME;
+
+		reaction_time2(reaction_crlp,reaction_crhp2,reaction_huhk);
+
+	}
 
 }
-
+//TODO FIX TIMINGS
 double reaction_time(int reaction_crmp, int reaction_crhp)
 {
 	PRINTF("Beginning reaction Calculation\n");
@@ -248,8 +315,6 @@ double reaction_time(int reaction_crmp, int reaction_crhp)
 	PRINTF("%d", storage);
 
 	PIT_Configure_interrupt_mode(0.001);
-	//TODO add new table to db, 2 frame link!!!!!!!
-
 
 	while(1)
 	{
@@ -349,12 +414,15 @@ double reaction_time(int reaction_crmp, int reaction_crhp)
 
 			sprintf(frame_ouput, "%d", frame);
 
+
 			strcat(output, ",");
 			strcat(output, output2);
 			strcat(output, ",");
 			strcat(output, frame_ouput);
 			strcat(output, ",");
 			strcat(output, frame_ouput);
+			strcat(output, ",");
+			strcat(output, "cr_mp_cr_mp_cr_hk");
 
 			if(failure_flag==1)
 			{
@@ -367,17 +435,158 @@ double reaction_time(int reaction_crmp, int reaction_crhp)
 			thisstate = RETURN;
 			break;
 		case RETURN:
+			thisstate = READY;
+			break;
 
 
-			//return reaction;
-			//for(int i=0;i<20;i++)
-			//{
-			//*data=(char)reaction;
-			///PRINTF("%c", data[i]);
-			//}
-			//*data = (char ) reaction;
-			//PRINTF("here");
-			//PRINTF("%c",*data);
+
+		}
+
+	}
+
+
+}
+double reaction_time2(int reaction_crlp, int reaction_crhp2, int reaction_huhk)
+{
+	//TODO wir up square, go through each bit again
+	PRINTF("Beginning reaction Calculation\n");
+	volatile int temp;
+	volatile int temp2;
+	float reaction;
+	float reaction2;
+	int frame;
+	int hitstun = 3*1000/TIME;//CR_LP
+	int hitstun2 = 2*1000/TIME;//CR_HP
+	char output[50];
+	char output2[50];
+
+	char frame_ouput[2];
+	int result =0;
+
+	storage = reaction_crlp;
+	storage2 = reaction_crhp2;
+	storage3 = reaction_huhk;
+
+	PIT_Configure_interrupt_mode(0.001);
+
+	while(1)
+	{
+
+		switch(thisstate)//better naming convention
+		{
+		case READY:
+
+			while(delay/1000>0)
+				PRINTF("\r%3d", --delay / 1000);
+			thisstate = PROCESS;
+			break;
+		case PROCESS:
+			PRINTF("\n");
+			delay=10000;
+			storage = reaction_crlp;
+			temp2 = reaction_crhp2;
+			sw7_count=0;//cr
+			sw6_count=0;//cr_lp
+			run=0;
+			while((sw6_count==0))
+			{}
+			run=1;
+			while(storage>0)
+			{
+				PRINTF("\r        %d", storage);
+			}
+			run=0;
+			sw6_count=0;
+			if(storage==0)
+			{
+				storage = reaction_crlp;
+				tick_count=0;
+				run=2;
+			}
+			while((sw6_count==0))
+			{}
+			run=1;
+			reaction = (float)(tick_count-hitstun)/1000;
+			int i;
+			if(reaction<0)
+			{
+				PRINTF("failure");
+				failure_flag =1;
+			}
+			else
+				PRINTF("\n%f", reaction);//CR_MP CR_MP next CR_MP CR_HP
+
+
+			frame = (reaction *60)/2;
+
+
+			PRINTF("frames: %d", frame);
+
+			temp = reaction;
+			float f2 = reaction - temp;
+			int d2 = (int)(f2 * 10000);
+			sprintf (output, "%d.%04d\n", temp, d2);
+			//tx_string(output);
+
+			storage = reaction_crlp;
+			//run=1;
+			PRINTF("\n");
+			while(storage>0)
+			{
+				PRINTF("\r        %d", storage);
+			}
+			if(storage==0)
+			{
+				storage = reaction_crlp;
+				tick_count=0;
+				run=2;
+			}
+			sw7_count=0;
+			sw6_count=0;
+
+			while((sw7_count==0))
+			{}
+			run=0;
+			reaction2 = (float)(tick_count-hitstun2)/1000;
+
+			if(reaction2<0)
+			{
+				PRINTF("failure");
+				failure_flag =1;
+			}
+			else
+				PRINTF("\n%f", reaction2);//CR_MP CR_MP next CR_MP CR_HP
+
+			frame = reaction *30;
+			PRINTF("frames: %d", frame);
+
+			temp2 = reaction2;
+			float f3 = reaction2 - temp2;
+			int d3 = (int)(f3 * 10000);
+			sprintf (output2, "%d.%04d\n", temp2, d3);
+
+			sprintf(frame_ouput, "%d", frame);
+
+			strcat(output, ",");
+			strcat(output, output2);
+			strcat(output, ",");
+			strcat(output, frame_ouput);
+			strcat(output, ",");
+			strcat(output, frame_ouput);
+			strcat(output, ",");
+			strcat(output, "cr_lp_cr_hp_hu_hk");
+
+			if(failure_flag==1)
+			{
+				tx_string("FAIL");
+				failure_flag=0;
+			}
+			else
+				tx_string(output);
+
+			thisstate = RETURN;
+			break;
+		case RETURN:
 			thisstate = READY;
 			break;
 
@@ -390,10 +599,7 @@ double reaction_time(int reaction_crmp, int reaction_crhp)
 
 }
 
-// assumes float is < 65536 and ARRAYSIZE is big enough
-// problem: it truncates numbers at size without rounding
-// str is a char array to hold the result, float is the number to convert
-// size is the number of decimal digits you want
+
 
 
 void PORTC_PORTD_IRQHandler()
@@ -404,11 +610,13 @@ void PORTC_PORTD_IRQHandler()
 		PORTC_ISFR|= SW3_MASK; //clear interrupt flag for ptc1
 
 		sw_count++;
+
 	}
 	if(PORTC_ISFR & SW2_MASK)
 	{
 		PORTC_ISFR|= SW2_MASK; //clear interrupt flag for ptc1
 		sw2_count++;
+
 	}
 	if(PORTC_ISFR & SW4_MASK)
 	{
@@ -426,6 +634,20 @@ void PORTC_PORTD_IRQHandler()
 	{
 		PORTD_ISFR|= SW6_MASK; //clear interrupt flag for ptc1
 		sw5_count++;
+
+
+
+	}
+	if(PORTD_ISFR & SW7_MASK)
+	{
+		PORTD_ISFR|= SW7_MASK; //clear interrupt flag for ptc1
+		sw6_count++; //x
+
+	}
+	if(PORTD_ISFR & SW8_MASK)
+	{
+		PORTD_ISFR|= SW8_MASK; //clear interrupt flag for ptc1
+		sw7_count++;
 
 	}
 
